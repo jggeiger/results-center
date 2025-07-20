3138755655595c93ce73a06237d364f6f2a00194
jggeiger <jggeiger@ualberta.ca>
2025-07-20 16:16:21 -0600

    simple success test for ResultsController

--- test/controllers/results_controller_test.rb
+++ test/controllers/results_controller_test.rb
@@ -1,7 +1,9 @@
 require "test_helper"
 
 class ResultsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get results page" do
+    election_id = Election.first.id
+    get "/results/" + election_id.to_s
+    assert_response :success
+  end
 end
