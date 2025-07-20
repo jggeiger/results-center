98867c6dacda2d155e54cf6a74dc4a906f01d791
jggeiger <jggeiger@ualberta.ca>
2025-07-20 15:35:11 -0600

    ballot fixture changed to use generated answer id versus hardcoded

--- test/models/question_test.rb
+++ test/models/question_test.rb
@@ -4,15 +4,18 @@ class QuestionTest < ActiveSupport::TestCase
   test "question result contains proper information" do
     question = questions(:first_board)
 
+    ans0 = answers(:answer4).id.to_s
+    ans1 = answers(:answer5).id.to_s
+
     expected = [
-      ["1"],
+      [ans1],
       {
-        "0" => 1,
-        "1" => 2
+        ans0 => 1,
+        ans1 => 2
       },
       {
-        "0" => 0.33,
-        "1" => 0.66
+        ans0 => 0.33,
+        ans1 => 0.66
       }
     ]
 
@@ -25,20 +28,22 @@ class QuestionTest < ActiveSupport::TestCase
   test "question result with nil ballots" do
     question = questions(:empty_ballots)
 
+    ans0 = answers(:answer8).id.to_s
+    ans1 = answers(:answer9).id.to_s
+
     expected = [
-      ["1"],
+      [ans0],
       {
-        "0" => 0,
-        "1" => 2
+        ans0 => 2,
+        ans1 => 0
       },
       {
-        "0" => 1.00,
-        "1" => 0
+        ans0 => 1.00,
+        ans1 => 0
       }
     ]
 
     result = Question.get_results(question.id)
-    puts "nil ballots", result
 
     assert_equal(expected, result, "question result does not contain correct information when some ballots are empty")
 
@@ -50,7 +55,6 @@ class QuestionTest < ActiveSupport::TestCase
     expected = [[], {}, {}]
 
     result = Question.get_results(question.id)
-    puts "no ballots", result
 
     assert_equal(expected, result, "question result does not contain correct information when no ballots cast")
 
@@ -59,20 +63,22 @@ class QuestionTest < ActiveSupport::TestCase
   test "question result with tie" do
     question = questions(:tie)
 
+    ans0 = answers(:answer12).id.to_s
+    ans1 = answers(:answer13).id.to_s
+
     expected = [
-      ["0","1"],
+      [ans0, ans1].sort,
       {
-        "0" => 2,
-        "1" => 2
+        ans0 => 2,
+        ans1 => 2
       },
       {
-        "0" => 0.50,
-        "1" => 0.50
+        ans0 => 0.50,
+        ans1 => 0.50
       }
     ]
 
     result = Question.get_results(question.id)
-    puts "tie", result
 
     assert_equal(expected, result, "question result does not contain correct information when tie occurs")
 
@@ -81,20 +87,22 @@ class QuestionTest < ActiveSupport::TestCase
   test "question result with an invalid ballot answer" do
     question = questions(:invalid_answer)
 
+    ans0 = answers(:answer14).id.to_s
+    ans1 = answers(:answer15).id.to_s
+
     expected = [
-      ["0"],
+      [ans0],
       {
-        "0" => 2,
-        "1" => 1
+        ans0 => 2,
+        ans1 => 1
       },
       {
-        "0" => 0.66,
-        "1" => 0.33,
+        ans0 => 0.66,
+        ans1 => 0.33,
       }
     ]
 
     result = Question.get_results(question.id)
-    puts "invalid_answer", result
 
     assert_equal(expected, result, "question result does not contain correct information when a ballot contains an invalid answer")
 
