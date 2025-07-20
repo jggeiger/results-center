1962a03f773c312f92f25a47fce1d75d70cac142
jggeiger <jggeiger@ualberta.ca>
2025-07-20 14:15:03 -0600

    added test cases for results calculator edge cases

--- test/models/election_test.rb
+++ test/models/election_test.rb
@@ -19,4 +19,31 @@ class ElectionTest < ActiveSupport::TestCase
     assert_includes(q_ids, question2.id, "Election results are missing a question")
 
   end
+
+  test "election results contains all relevant questions when edge cases present" do
+    board_edge = elections(:board_edge)
+
+    tie = questions(:tie)
+    empty_ballots = questions(:empty_ballots)
+    invalid_answer = questions(:invalid_answer)
+
+    results = JSON[Election.get_results(board_edge.id)]
+    q_ids = []
+    results["questions"].each { |q|
+      q_ids.append(q["id"])
+    }
+
+    assert_includes(q_ids, tie.id, "Election results are missing question where tie occurs")
+    assert_includes(q_ids, empty_ballots.id, "Election results are missing question where empty ballots present")
+    assert_includes(q_ids, invalid_answer.id, "Election results are missing question where invalid answer present")
+
+    no_ballots = elections(:no_ballots)
+    no_ballots_question = questions(:no_ballots)
+
+    results = JSON[Election.get_results(no_ballots.id)]
+    q_id = results["questions"][0]["id"]
+
+    assert_equal(q_id, no_ballots_question.id, "Election results are missing question when no ballots")
+
+  end
 end
--- test/models/question_test.rb
+++ test/models/question_test.rb
@@ -21,4 +21,83 @@ class QuestionTest < ActiveSupport::TestCase
     assert_equal(expected, result, "question result does not contain correct information")
 
   end
+
+  test "question result with nil ballots" do
+    question = questions(:empty_ballots)
+
+    expected = [
+      ["1"],
+      {
+        "0" => 0,
+        "1" => 2
+      },
+      {
+        "0" => 1.00,
+        "1" => 0
+      }
+    ]
+
+    result = Question.get_results(question.id)
+    puts "nil ballots", result
+
+    assert_equal(expected, result, "question result does not contain correct information when some ballots are empty")
+
+  end
+
+  test "question result with no ballots" do
+    question = questions(:no_ballots)
+    
+    expected = [[], {}, {}]
+
+    result = Question.get_results(question.id)
+    puts "no ballots", result
+
+    assert_equal(expected, result, "question result does not contain correct information when no ballots cast")
+
+  end
+
+  test "question result with tie" do
+    question = questions(:tie)
+
+    expected = [
+      ["0","1"],
+      {
+        "0" => 2,
+        "1" => 2
+      },
+      {
+        "0" => 0.50,
+        "1" => 0.50
+      }
+    ]
+
+    result = Question.get_results(question.id)
+    puts "tie", result
+
+    assert_equal(expected, result, "question result does not contain correct information when tie occurs")
+
+  end
+
+  test "question result with an invalid ballot answer" do
+    question = questions(:invalid_answer)
+
+    expected = [
+      ["0"],
+      {
+        "0" => 2,
+        "1" => 1
+      },
+      {
+        "0" => 0.66,
+        "1" => 0.33,
+      }
+    ]
+
+    result = Question.get_results(question.id)
+    puts "invalid_answer", result
+
+    assert_equal(expected, result, "question result does not contain correct information when a ballot contains an invalid answer")
+
+  end
+
 end
