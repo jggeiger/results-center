e3485e882bd298b2c53a0bec00dc2b9d0ba3088f
jggeiger <jggeiger@ualberta.ca>
2025-07-20 12:58:36 -0600

    added basic test cases for results calculator

--- test/models/election_test.rb
+++ test/models/election_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 class ElectionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "election results contain all relevant questions" do
+    
+    board = elections(:board)
+
+    question1 = questions(:first_board)
+    question2 = questions(:second_board)
+
+    results = JSON[Election.get_results(board.id)]
+
+    q_ids = []
+    results["questions"].each { |q|
+      q_ids.append(q["id"])
+    }
+
+    assert_includes(q_ids, question1.id, "Election results are missing a question")
+    assert_includes(q_ids, question2.id, "Election results are missing a question")
+
+  end
 end
--- test/models/question_test.rb
+++ test/models/question_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
 class QuestionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "question result contains proper information" do
+    question = questions(:first_board)
+
+    expected = [
+      ["1"],
+      {
+        "0" => 1,
+        "1" => 2
+      },
+      {
+        "0" => 0.33,
+        "1" => 0.66
+      }
+    ]
+
+    result = Question.get_results(question.id)
+    
+    assert_equal(expected, result, "question result does not contain correct information")
+
+  end
 end
